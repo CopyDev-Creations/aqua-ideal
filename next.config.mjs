c8b8d2cda44ba8b5ae958b8ddc877081ca2385f1
Mariusz Szlęzak <wars5432@gmail.com>
2024-06-19 00:07:24 +0200

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
+const isProduction = process.env.NODE_ENV == 'production'
+
 const nextConfig = {
+    basePath: isProduction ? "/aqua-ideal" : "",
     output: "export",
     reactStrictMode: false,
     images: { unoptimized: true }
